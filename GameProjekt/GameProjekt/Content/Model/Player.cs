edf64e96ac5d1c9f5ec6f3e8612156649fb348eb
ej222pj <ej222pj@student.lnu.se>
2014-12-13 15:07:24 +0100

    Snurrisnurr

--- GameProjekt/GameProjekt/Content/Model/Player.cs
+++ GameProjekt/GameProjekt/Content/Model/Player.cs
@@ -14,6 +14,8 @@ namespace GameProjekt.Content.Model
         private Texture2D texture;
         private Vector2 position = new Vector2(352, 1536);
         private Vector2 velocity;
+        Vector2 rotate;
+        Vector2 direction;
         private Rectangle rectangle;
         KeyboardState oldState;
         DragLine dragLine;
@@ -22,10 +24,14 @@ namespace GameProjekt.Content.Model
         private bool shootLine = false;
         private bool isRotating = false;
         private bool rotationDirection = false;
+        private bool beforeFirstRotation = false;
         private int tileSize;
         private float currentAngle = 0;
-        private float angleStep = 0.105f;
+        private float angleStep = 0.05f;
         private float playerAngle = 0;
+        private float distance;
+        Vector2 tangent1;
+        Vector2 tangent2;
 
         public bool ShootLine 
         {
@@ -48,12 +54,12 @@ namespace GameProjekt.Content.Model
             texture = Content.Load<Texture2D>("Tiles/Player");
         }
 
-        public void Update(GameTime gameTime, Vector2 playerPosition, Vector2 center) 
+        public void Update(GameTime gameTime, Vector2 center) 
         {
             position += velocity;
             if (shootLine)
             {
-                Vector2 rotate = Rotate(playerPosition, center);
+                rotate = Rotate(position, center);
                 
                 position = new Vector2(rotate.X, rotate.Y);
                 rectangle = new Rectangle((int)rotate.X, (int)rotate.Y, tileSize, tileSize);
@@ -61,18 +67,31 @@ namespace GameProjekt.Content.Model
             }
             else
             {
+                rotate = new Vector2(position.X, position.Y);
                 rectangle = new Rectangle((int)position.X, (int)position.Y, tileSize, tileSize);
                 isRotating = false;
             }
-            Input(gameTime, playerPosition, center);
+            Input(gameTime, position, center);
 
             if (isRotating) 
             {
                 velocity.Y = 0f;
             }
-            else if (hasStarted) 
+            else if (hasStarted && !beforeFirstRotation) 
             {
-                velocity.Y = -2.0f;
+                velocity.Y = -0.7f;
+            }
+            else if (hasStarted && beforeFirstRotation)
+            {
+                if (center.X < position.X)
+                {
+                    velocity = tangent2;
+                }
+                else if (center.X > position.X)
+                {
+                    velocity = tangent1;
+                }
+                 
             }
            
         }
@@ -106,6 +125,7 @@ namespace GameProjekt.Content.Model
                 // If not down last update, key has just been pressed.
                 if (!oldState.IsKeyDown(Keys.Space))
                 {
+                    beforeFirstRotation = true;
                     if (center.X < playerPosition.X)
                     {
                         rotationDirection = true;
@@ -114,7 +134,7 @@ namespace GameProjekt.Content.Model
                     {
                         rotationDirection = false;
                     }
-
+                    ReleaseRotation(center, distance, rotate);
                     shootLine = !shootLine;
                 }
             }
@@ -131,7 +151,31 @@ namespace GameProjekt.Content.Model
             {
                 dragLine.IsConnected = false; 
             }
-        }       
+        }
+
+        public void ReleaseRotation(Vector2 circle_center, float circle_radius, Vector2 point)
+        {
+            //http://www.gamedev.net/topic/499818-tangents-to-a-circle-through-a-point/ skrivet av user: Swiftcoder
+            //def find_tangents_through_point():
+            //#find the direction from the point to the center of the circle
+            Vector2 dir = point - circle_center;
+            //#extract the length and angle
+            float len = dir.Length();
+            float angle = (float)Math.Atan2(dir.Y, dir.X);
+
+            //# derive the length of the tangent using pythagoras
+            float tangent_len = (float)Math.Sqrt(len * 2 - circle_radius * 2);
+            //# and the angle using trigonometry
+            float tangent_angle = (float)Math.Asin(circle_radius / len);
+
+            //# there are 2 tangents, one either side
+            float pos = angle + tangent_angle;
+            float neg = angle - tangent_angle;
+
+            //#return the direction vector of each tanget (the starting point was passed in)
+            tangent1 = new Vector2((float)Math.Cos(pos), (float)Math.Sin(pos));
+            tangent2 = new Vector2((float)Math.Cos(neg), (float)Math.Sin(neg));
+        }
 
         public void Collision(Rectangle newRectangle) 
         {
@@ -183,23 +227,32 @@ namespace GameProjekt.Content.Model
 
             float XDistance = currentPos.X - centre.X;
             float YDistance = currentPos.Y - centre.Y;         
-            float distancee = (float)Math.Sqrt(XDistance * XDistance + YDistance * YDistance);
+            distance = (float)Math.Sqrt(XDistance * XDistance + YDistance * YDistance);
 
             float yDifference = (float)Math.Sin(currentAngle);
             float xDifference = (float)Math.Cos(currentAngle);
-            Vector2 direction = new Vector2(xDifference, yDifference);
-            Vector2 newPosition = centre + direction * distancee;
-            Console.WriteLine(currentPos);
+            direction = new Vector2(xDifference, yDifference);
+            Vector2 newPosition = centre + direction * distance;
+            Console.WriteLine(direction);
             return newPosition;   
         }
 
         public void ResetGame() 
         {
             position = new Vector2(352, 1536);
+            rotate = position;
             velocity.Y = 0.0f;
             velocity.X = 0.0f;
             hasStarted = false;
             shootLine = false;
+            beforeFirstRotation = false;
+            playerAngle = 0;
+            currentAngle = 0;
+            tangent1 = new Vector2(0,0);
+            tangent2 = new Vector2(0, 0);
+            isRotating = false;
+            rotationDirection = false;
+            
         }
 
         public void Draw(SpriteBatch spriteBatch) 
@@ -208,10 +261,8 @@ namespace GameProjekt.Content.Model
             Color color = new Color(0, 0, 0, 0);
 
             Vector2 origin = new Vector2(texture.Bounds.Width / 2, texture.Bounds.Height / 2);
-            //spriteBatch.Draw(texture, rectangle, Color.White);
             spriteBatch.Draw(texture, rectangle, new Rectangle(0, 0, texture.Width, texture.Height), Color.White, playerAngle, origin, SpriteEffects.None, 0);
-            //spriteBatch.Draw(smokeTexture, camera.scaleParticle(position.X, position.Y, smokeSize), new Rectangle(0, 0, smokeTexture.Bounds.Width, smokeTexture.Bounds.Height), color, rotation, origin, SpriteEffects.None, 0);
-        }
+       }
 
         
     }
